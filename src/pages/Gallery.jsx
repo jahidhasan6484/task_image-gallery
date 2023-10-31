022e58f78cf2032d593cbe81a5c34fed5b68017b
jahidhasan6484 <jahid@lintechgroup.co.uk>
2023-10-31 23:27:36 +0600

    yo

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,12 +1,46 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import imagesData from "../assets/data";
 
 const Gallery = () => {
   const [images, setImages] = useState([]);
 
+  const dragStartIndex = useRef(null);
+  const dragEndIndex = useRef(null);
+
   useEffect(() => {
     setImages(imagesData);
   }, []);
+
+  const handleDragStart = (_index) => {
+    dragStartIndex.current = _index;
+  };
+  const handleDragOver = (_index) => {
+    dragEndIndex.current = _index;
+
+    // dropAndReplaceImage();
+
+    const _allImages = [...images];
+
+    const draggedImage = _allImages.splice(dragStartIndex.current, 1)[0];
+    _allImages.splice(dragEndIndex.current, 0, draggedImage)[0];
+
+    // dragStartIndex.current = null;
+    // dragEndIndex.current = null;
+
+    setImages(_allImages);
+  };
+
+  const dropAndReplaceImage = () => {
+    const _allImages = [...images];
+
+    const draggedImage = _allImages.splice(dragStartIndex.current, 1)[0];
+    _allImages.splice(dragEndIndex.current, 0, draggedImage)[0];
+
+    dragStartIndex.current = null;
+    dragEndIndex.current = null;
+
+    setImages(_allImages);
+  };
 
   return (
     <div>
@@ -16,10 +50,14 @@ const Gallery = () => {
         {images.map((image, index) => {
           return (
             <div
+              draggable
               key={index}
               className={
                 index === 0 ? "col-span-2 row-span-2 border-2" : "border-2"
               }
+              onDragStart={() => handleDragStart(index)}
+              onDragEnter={() => handleDragOver(index)}
+              onDragEnd={() => dropAndReplaceImage()}
             >
               <img src={image} alt={image}></img>
             </div>
